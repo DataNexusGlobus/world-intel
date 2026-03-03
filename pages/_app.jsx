0a4d8c69d8462536a22d9d6a849cd263fbe2c3f4
DataNexusGlobus <datanexusglobus@gmail.com>
2026-03-03 18:06:55 +0530

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,9 @@
 import '../styles/globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+   return <>
+    <Component {...pageProps} />
+    <SpeedInsights />
+  </>;
 }

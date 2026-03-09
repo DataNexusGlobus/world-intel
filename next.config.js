533abbd37a54e834e8b2d43f3d729510bf210015
DataNexusGlobus <datanexusglobus@gmail.com>
2026-03-09 23:58:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    // Don't try to bundle kokoro-js on the server side
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'kokoro-js'];
+    }
+    // Handle WebGPU shader files inside kokoro-js
+    config.module.rules.push({
+      test: /\.wgsl$/i,
+      type: 'asset/source',
+    });
     return config;
   },
 };
